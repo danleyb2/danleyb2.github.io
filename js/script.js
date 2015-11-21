c052922333eeda764deadc9c5d36e7f5a5c28502
danleyb2 <ndieksman@gmail.com>
2015-11-21 19:12:06 +0300

    add googlemaps config

--- js/script.js
+++ js/script.js
@@ -23,3 +23,38 @@ function sendMessage(t){
         "message":t.target.message.value.trim()
     });
 }
+// Google Maps Scripts
+// When the window has finished loading create our google map below
+google.maps.event.addDomListener(window, 'load', init);
+function init() {
+    // Basic options for a simple Google Map
+    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+    var mapOptions = {
+        // How zoomed in you want the map to start at (always required)
+        zoom: 15,
+
+        // The latitude and longitude to center the map (always required)
+        center: new google.maps.LatLng(-0.6799689999999999, 35.020655),
+        mapTypeId: google.maps.MapTypeId.SATELLITE,
+        // Disables the default Google Maps UI components
+        disableDefaultUI: false,
+        scrollwheel: false,
+        draggable: false
+    };
+
+    // Get the HTML DOM element that will contain your map
+    // We are using a div with id="map" seen below in the <body>
+    var mapElement = document.getElementById('map');
+
+    // Create the Google Map using out element and options defined above
+    var map = new google.maps.Map(mapElement, mapOptions);
+
+    // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
+    var image = 'img/me/marker.png';
+    var myLatLng = new google.maps.LatLng(-0.6799689999999999, 35.020655);
+    var beachMarker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        icon: image
+    });
+}
